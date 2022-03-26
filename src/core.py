668766b0cb6e61a5afa2922b0885679cd89f0dc0
D3vil0per <crazysnob@live.it>
2022-03-26 21:18:28 +0100

    Update core.py

--- src/core.py
+++ src/core.py
@@ -170,15 +170,19 @@ def main():
     if args.now and not args.algorithm:
         print("Date time  [local time]: "+str(verter.current_local_date()))
         print("Epoch time [local time]: "+str(verter.current_local_epoch())) #The output is a little skewed because the two functions are called in two different times
+        exit()
 
     if args.utc and not args.algorithm:
         print("Date time  [UTC%s]: " % args.utc +str(verter.current_utc_date(args.utc)))
         print("Epoch time [UTC%s]: " % args.utc +str(verter.current_utc_epoch(args.utc)))
+        exit()
 
     if args.date and not args.algorithm:
         print("Epoch time: "+str(verter.date_to_epoch(args.date)))
+        exit()
 
     if args.epoch and not args.algorithm:
         print("Date time:  "+verter.epoch_to_date(args.epoch))
+        exit()
 
     token_request(args)
